0c55fbf05082929e17dff3ab47022f2afba34ecc
Nairod97425 <dorian.hoa97425@gmail.com>
2025-10-24 15:28:34 +0400

    rajout link

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
     <title>EduAI - Ton assistant IA pour réussir</title>
 </head>
 <body>
